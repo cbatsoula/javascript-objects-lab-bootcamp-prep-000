69c76c747af949375866ce8caf71ec8f5db43877
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-06 20:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
    return object ;
 }
 function deleteFromObjectByKey(object, key){
- var newObj = Object.assign({}, object, [key]);
+ var newObj = Object.assign({recipes}, object, [key]);
   delete newObj.key;
   
 }
